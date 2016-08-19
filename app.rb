1ff5af8fdf779f6e9f53c7db17c953a75b6d19ce
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2016-08-19 15:50:01 +0100

    Create PivotalPoster class and add Pivotal Tracker authentication

--- app.rb
+++ app.rb
@@ -1,17 +1,14 @@
 require 'sinatra/base'
 require 'dotenv'
 Dotenv.load
-require './lib/github_pull_request'
 require 'json'
 
-class GDSGithubPivotal < Sinatra::Base
+class GDSGithubTrello < Sinatra::Base
 
   post '/payload' do
     status 204
     body ''
-    pivotal_poster = PivotalPoster.new
     payload = JSON.parse(request.body.read)
-    GitHubPullRequest.new(payload["repository"]["id"],payload["number"],pivotal_poster)
   end
 
   # start the server if ruby file executed directly
