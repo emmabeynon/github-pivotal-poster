67e8d3b69590fad95348ed402146b542ef2c4b10
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2016-08-26 10:36:57 +0100

    Retrive a given project from PivotalTracker

--- lib/pivotal_poster.rb
+++ lib/pivotal_poster.rb
@@ -1,11 +1,18 @@
 require 'pivotal-tracker'
 
 class PivotalPoster
-  attr_reader :client
+  attr_reader :client, :project
 
-  def initialize
+  def initialize(project_id)
     @client = authenticate
+    @project = retrieve_project(project_id)
   end
+
+  def retrieve_project(project_id)
+    PivotalTracker::Project.find(project_id)
+  end
+
+private
 
   def authenticate
     PivotalTracker::Client.token = ENV['PIVOTAL_API_TOKEN']
--- spec/pivotal_poster_spec.rb
+++ spec/pivotal_poster_spec.rb
@@ -1,11 +1,18 @@
 require 'pivotal_poster'
 
 describe PivotalPoster do
-  let(:pivotal_poster) { PivotalPoster.new }
+  let(:pivotal_poster) { PivotalPoster.new(1788875) }
+  let(:pivotal_project) do
+    instance_double("PivotalTracker::Project", id: 1788875, name: "Test project")
+  end
 
-  describe '#authenticate' do
-    it 'authenticates a PivotalTracker client' do
-      expect(pivotal_poster.client).not_to be nil
+  before(:each) do
+    allow(PivotalTracker::Project).to receive(:find).and_return(pivotal_project)
+  end
+
+  describe '#retrieve_project' do
+    it 'retrieves the correct PivotalTracker project' do
+      expect(pivotal_poster.project).to eq(pivotal_project)
     end
   end
 end
