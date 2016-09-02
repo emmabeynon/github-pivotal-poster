69d9a767a8c3b63ebf5ed0baeb68d9e91369fb7d
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2016-09-02 09:58:19 +0100

    Configure Sinatra app

--- app.rb
+++ app.rb
@@ -2,13 +2,16 @@ require 'sinatra/base'
 require 'dotenv'
 Dotenv.load
 require 'json'
+require './lib/github_pull_request'
 
-class GDSGithubTrello < Sinatra::Base
+class GithubPivotalPoster < Sinatra::Base
 
   post '/payload' do
     status 204
     body ''
+    pivotal_poster = PivotalPoster.new(ENV['PIVOTAL_API_TOKEN'])
     payload = JSON.parse(request.body.read)
+    GitHubPullRequest.new(payload["repository"]["id"],payload["number"],pivotal_poster)
   end
 
   # start the server if ruby file executed directly
